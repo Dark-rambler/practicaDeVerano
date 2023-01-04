6ff60eb20a710c69eb54939f6bad062ac8c534a2
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-01-04 17:22:11 -0400

    primera estructura

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+//import reactLogo from './assets/react.svg'
+//import './App.css'
+import Landing from './components/landing/Landing'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Landing/>
     </div>
   )
 }
---
+++ frontEnd/src/components/landing/Landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import'../../styles/landing/Landing.css'
+function Landing(){
+    return(  <div className="container">
+        hola
+    </div>
+
+    )
+}
+
+export default Landing
